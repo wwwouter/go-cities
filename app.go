a22191b47e160fd098254d0a27c66fb82379f3f4
wwwouter <wouter@wercker.com>
2013-08-21 14:24:22 +0200

    Update app.go

--- app.go
+++ app.go
@@ -1,30 +1,23 @@
-package main
+package hello
 
 import (
 	"encoding/json"
-	"flag"
 	"fmt"
 	"net/http"
 )
 
 var (
-	Address = flag.String("address", "", "the address to host on")
-	Port    = flag.Int("port", 80, "the port to host on")
-	cities  = []string{
+	cities = []string{
 		"Amsterdam", "San Francisco", "Paris", "New York", "Portland",
 	}
 )
 
-func main() {
-	flag.Parse()
-	endpoint := fmt.Sprintf("%v:%v", *Address, *Port)
+func init() {
+	r := http.NewServeMux()
+	r.HandleFunc("/", handleIndex)
+	r.HandleFunc("/ping", pingHandler)
 
-	http.HandleFunc("/", handleIndex)
-
-	fmt.Printf("Hosting at %v\n", endpoint)
-	if err := http.ListenAndServe(endpoint, nil); err != nil {
-		fmt.Printf("Error: %v", err)
-	}
+	http.Handle("/", r)
 }
 
 func handleIndex(rw http.ResponseWriter, req *http.Request) {
@@ -32,4 +25,8 @@ func handleIndex(rw http.ResponseWriter, req *http.Request) {
 
 	encoder := json.NewEncoder(rw)
 	encoder.Encode(cities)
+}
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Pong!")
 }
